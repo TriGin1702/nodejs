88a083c4ade71b1415472a7fa29e31f6946e82ee
Minh Tri <luuminhtri17022001@gmail.com>
2024-06-11 04:37:01 +0700

    update comment

---
+++ src/app/control/connect.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql2");
+const connect = mysql.createConnection({
+    host: "mysql-db",
+    port: "3306",
+    user: "root",
+    password: "gin@17022001",
+    database: "web",
+});
+// const connect = mysql.createConnection({
+//     host: "localhost", // Tên của service trong Docker Compose
+//     port: "3309", // Port mặc định của MySQL
+//     user: "root",
+//     password: "gin@17022001",
+//     database: "web",
+// });
+// const connect = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "web",
+//     keepAliveInitialDelay: 10000, // 0 by default.
+//     enableKeepAlive: true,
+// });
+connect.connect((err) => {
+    if (err) {
+        console.error("Error connecting to database:", err);
+        return;
+    }
+    console.log("Connected to database");
+});
+module.exports = connect;
